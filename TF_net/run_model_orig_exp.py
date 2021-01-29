Added data normalization before loading in the data and correspondingly fixed the Evaluation-Raw notebook for the plots for experiments that were run on unnormalized data

TF_net/run_model_orig_exp.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from torch.utils import data
+import torchvision.transforms as transforms
 import itertools
 import argparse
 import re
@@ -26,6 +27,9 @@ parser.add_argument('--model_name', type=str, dest='model_name', default='model'
 parser.add_argument('--result_name', type=str, dest='result_name', default='result', help='file name to save results')
 parser.add_argument('--gpu', type=int, dest='gpu', default=0, help='GPU ID to use')
 parser.add_argument('--num_workers', type=int, dest='num_workers', default=8, help='Number of worker processes to use to load data')
+parser.add_argument('--dir', type=str, dest='dir', default='./TF_net/Data/samples/', help='directory where the data is')
+parser.add_argument('--orig_norm', action='store_true', help='Indicate to use the original mean and std from the paper to normalize with'
+parser.add_argument('--test', action='store_true', help='Just load a saved model to test')
 args = parser.parse_args()
 
 device = torch.device("cuda:"+str(args.gpu) if torch.cuda.is_available() else "cpu")
@@ -37,6 +41,18 @@ device = torch.device("cuda:"+str(args.gpu) if torch.cuda.is_available() else "c
 #test_direc = "/global/cscratch1/sd/rwang2/TF-net/Data/data_64/sample_"
 train_direc = './TF_net/Data/samples/'
 test_direc = './TF_net/Data/samples/'
+
+ORIG_AVG = 1.0424337
+ORIG_STDS = 4522.7046
+
+# Normalize the images!! Get the per-channel mean and std for training images and use that to normalize all images (remember z = (x - mean) / std)
+def get_train_avg_std(train_indices):
+    full = torch.load('./TF_net/Data/rbc_data.pt')
+    chan1_mean = full[train_indices[0]:train_indices[-1], 0, :, :].mean()
+    chan1_std = full[train_indices[0]:train_indices[-1], 0, :, :].std()
+    chan2_mean = full[train_indices[0]:train_indices[-1], 1, :, :].mean()
+    chan2_std = full[train_indices[0]:train_indices[-1], 1, :, :].std()      
+    return (chan1_mean, chan1_std, chan2_mean, chan2_std)
 
 #best_params: kernel_size 3, learning_rate 0.001, dropout_rate 0, batch_size 120, input_length 25, output_length 4
 min_mse = 1
@@ -52,65 +68,78 @@ batch_size = 32
 train_indices = list(range(0, 10000))
 #train_indices = [1]
 #valid_indices = list(range(6000, 7700))
-valid_indices = list(range(10000, 11790))
+valid_indices = list(range(10000, 11700))
 #valid_indices = [2]
 #test_indices = [3]
-test_indices = list(range(11790, 13580))
+test_indices = list(range(11700, 13300))
 #test_indices = list(range(7700, 9800))
 
-model = LES(input_channels = input_length*2, output_channels = 2, kernel_size = kernel_size, 
+print(f'data path : {args.dir}')
+
+if not args.test:
+    model = LES(input_channels = input_length*2, output_channels = 2, kernel_size = kernel_size, 
             dropout_rate = dropout_rate, time_range = time_range).to(device)
-# DataParallel requires input tensor to be provided on first device in device_ids list, so need to prepend current cuda device
-default_list = _get_all_device_indices()
-device_ids = [args.gpu] + default_list
-model = nn.DataParallel(model, device_ids=device_ids)
+    # DataParallel requires input tensor to be provided on first device in device_ids list, so need to prepend current cuda device
+    default_list = _get_all_device_indices()
+    device_ids = [args.gpu] + default_list
+    model = nn.DataParallel(model, device_ids=device_ids)
 
-# Note that when generating the data, they need to be batches of at least size 46(mid<40>+output_length), probably should try 50
-#train_set = Dataset(valid_indices, input_length + time_range - 1, 40, output_length, train_direc, True)
-train_set = Dataset(train_indices, input_length + time_range - 1, 40, output_length, train_direc, True)
-valid_set = Dataset(valid_indices, input_length + time_range - 1, 40, 6, test_direc, True)
-train_loader = data.DataLoader(train_set, batch_size = batch_size, shuffle = True, num_workers = args.num_workers)
-valid_loader = data.DataLoader(valid_set, batch_size = batch_size, shuffle = False, num_workers = args.num_workers)
-#print(f'size of dataset example: {train_set[0].size()}')
+    # Note that when generating the data, they need to be batches of at least size 46(mid<40>+output_length), probably should try 50
+    #train_set = Dataset(valid_indices, input_length + time_range - 1, 40, output_length, train_direc, True)
+    # Create Normalization transform
+    if args.orig_norm:
+        trans_func = transforms.Compose([transforms.Normalize(mean=[ORIG_AVG, ORIG_AVG], std=[ORIG_STD, ORIG_STD])])
+    else:
+        chan1_mean, chan1_std, chan2_mean, chan2_std = get_train_avg_std(train_indices)
+        print(f'channel 0 mean {chan1_mean} and std: {chan1_std}')
+        print(f'channel 1 mean {chan2_mean} and std: {chan2_std}')
+        trans_func = transforms.Compose([transforms.Normalize(mean=[chan1_mean, chan2_mean], std=[chan1_std, chan2_std])])
 
-loss_fun = torch.nn.MSELoss()
-regularizer = DivergenceLoss(torch.nn.MSELoss())
-coef = 0
+    # Note that both the input feature and label are transformed/normalized
+    train_set = Dataset(train_indices, input_length + time_range - 1, 40, output_length, args.dir, True, transform=trans_func)
+    valid_set = Dataset(valid_indices, input_length + time_range - 1, 40, 6, args.dir, True, transform=trans_func)
+    train_loader = data.DataLoader(train_set, batch_size = batch_size, shuffle = True, num_workers = args.num_workers)
+    valid_loader = data.DataLoader(valid_set, batch_size = batch_size, shuffle = False, num_workers = args.num_workers)
+    #print(f'size of dataset example: {train_set[0].size()}')
 
-optimizer = torch.optim.Adam(model.parameters(), learning_rate, betas = (0.9, 0.999), weight_decay = 4e-4)
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size = 1, gamma = 0.9)
+    loss_fun = torch.nn.MSELoss()
+    regularizer = DivergenceLoss(torch.nn.MSELoss())
+    coef = 0
 
-train_mse = []
-valid_mse = []
-test_mse = []
-save = False
-for i in tqdm(range(100)):
-    print(f'Epoch {i}...')
-    start = time.time()
-    torch.cuda.empty_cache()
-    scheduler.step()
-    model.train()
-    train_mse.append(train_epoch(train_loader, model, optimizer, loss_fun, device, coef, regularizer))#
-    model.eval()
-    mse, preds, trues = eval_epoch(valid_loader, model, loss_fun, device)
-    valid_mse.append(mse)
-    if valid_mse[-1] < min_mse:
-        min_mse = valid_mse[-1] 
-        best_model = model 
-        torch.save(best_model, f"./checkpoints/{args.model_name}.pth")
-        save = True
-    end = time.time()
-    if (len(train_mse) > 50 and np.mean(valid_mse[-5:]) >= np.mean(valid_mse[-10:-5])):
+    optimizer = torch.optim.Adam(model.parameters(), learning_rate, betas = (0.9, 0.999), weight_decay = 4e-4)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size = 1, gamma = 0.9)
+
+    train_mse = []
+    valid_mse = []
+    test_mse = []
+    save = False
+    for i in tqdm(range(100)):
+        print(f'Epoch {i}...')
+        start = time.time()
+        torch.cuda.empty_cache()
+        scheduler.step()
+        model.train()
+        train_mse.append(train_epoch(train_loader, model, optimizer, loss_fun, device, coef, regularizer))#
+        model.eval()
+        mse, preds, trues = eval_epoch(valid_loader, model, loss_fun, device)
+        valid_mse.append(mse)
+        if valid_mse[-1] < min_mse:
+            min_mse = valid_mse[-1] 
+            best_model = model 
+            torch.save(best_model, f"./checkpoints/{args.model_name}.pth")
+            save = True
+        end = time.time()
+        if (len(train_mse) > 50 and np.mean(valid_mse[-5:]) >= np.mean(valid_mse[-10:-5])):
             break
-    print(train_mse[-1], valid_mse[-1], round((end-start)/60,5))
-print(time_range, min_mse)
+        print(train_mse[-1], valid_mse[-1], round((end-start)/60,5))
+    print(time_range, min_mse)
 
-if not save:
-    torch.save(model, f"./checkpoints/{args.model_name}.pth")
+    if not save:
+        torch.save(model, f"./checkpoints/{args.model_name}.pth")
 
 loss_fun = torch.nn.MSELoss()
 best_model = torch.load(f"./checkpoints/{args.model_name}.pth")
-test_set = Dataset(test_indices, input_length + time_range - 1, 40, 60, test_direc, True)
+test_set = Dataset(test_indices, input_length + time_range - 1, 40, 60, args.dir, True, transform=trans_func)
 test_loader = data.DataLoader(test_set, batch_size = batch_size, shuffle = False, num_workers = args.num_workers)
 preds, trues, loss_curve = test_epoch(test_loader, best_model, loss_fun, device)
 

TF_net/train_orig_exp.py
@@ -17,13 +17,14 @@ import kornia
 warnings.filterwarnings("ignore")
 
 class Dataset(data.Dataset):
-    def __init__(self, indices, input_length, mid, output_length, direc, stack_x):
+    def __init__(self, indices, input_length, mid, output_length, direc, stack_x, transform=None):
         self.input_length = input_length
         self.mid = mid
         self.output_length = output_length
         self.stack_x = stack_x
         self.direc = direc
         self.list_IDs = indices
+        self.transform = transform
         
     def __len__(self):
         return len(self.list_IDs)
@@ -34,6 +35,11 @@ class Dataset(data.Dataset):
             loaded_tensor = torch.load(self.direc + f"samples_{ID}.pt")
         except Exception as ex:
             print("There's an exception occurring when loading {}: {}".format(f'samples_{ID}.pt', str(ex)))
+        
+        if self.transform is not None:
+            loaded_tensor = self.transform(loaded_tensor)
+            # Normalize label as well
+
         y = loaded_tensor[self.mid:(self.mid+self.output_length)]
         if self.stack_x:
             #x = torch.load(self.direc + str(ID) + ".pt")[(self.mid-self.input_length):self.mid].reshape(-1, y.shape[-2], y.shape[-1])
@@ -41,9 +47,9 @@ class Dataset(data.Dataset):
         else:
             #x = torch.load(self.direc + str(ID) + ".pt")[(self.mid-self.input_length):self.mid]
             x = loaded_tensor[(self.mid-self.input_length):self.mid]
+        
         #y = torch.load(self.direc + str(ID) + ".pt")[self.mid:(self.mid+self.output_length)]
         #y = torch.load(self.direc + "rbc_data.pt")[self.mid:(self.mid+self.output_length)]
-        #print(f'x size: {x.size()}, y size: {y.size()}')
         return x.float(), y.float()
     
 def train_epoch(train_loader, model, optimizer, loss_function, device, coef = 0, regularizer = None):
@@ -106,7 +112,7 @@ def test_epoch(test_loader, model, loss_function, device):
     trues = []
     with torch.no_grad():
         loss_curve = []
-        print(f'number of examples in loader: {len(test_loader)}')
+        print(f'number of examples in test loader: {len(test_loader)}')
         for xx, yy in test_loader:
             xx = xx.to(device)
             yy = yy.to(device)